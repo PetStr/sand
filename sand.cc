d029063512802ef20faaf7e6dca6e036419faeaf
Petter Strandh <petter.strandh@gmail.com>
2021-12-31 12:01:30 +0100

    Trying to understand how exceptions work

--- sand.cc
+++ sand.cc
@@ -1,14 +1,33 @@
 #include <iostream>
+using std::cout;
 
+class co
+{
+public:
+    co() { cout << "co c_tor\n"; }
+    ~co() { cout << "co d_tor\n"; }
+};
 
-int main () {
-  try
-  {
-    throw 20;
-  }
-  catch (int e)
-  {
-    std::cout << "An exception occurred. Exception Nr. " << e << '\n';
-  }
-  return 0;
+class go
+{
+public:
+    go() { cout << "go c_tor\n"; }
+    ~go() { cout << "go d_tor\n"; }
+};
+
+int main()
+{
+    co b;
+    try
+        {
+            go a;
+            throw 20;
+            cout << "After throw\n";
+        }
+    catch (int e)
+        {
+            cout << "An exception occurred. Exception Nr. " << e << '\n';
+        }
+    cout << "b4 exit\n";
+    return 0;
 }
